c8887f3ea5aa053110b7d02d3a29a5c4a6730d0e
Asher D <s015847@students.lmsd.org>
2020-02-20 09:29:20 -0500

    game files!!!!!

---
+++ Quleo/GameScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameScene.swift
+//  Quleo
+//
+//  Created by Asher Dayanim (student LM) on 2/20/20.
+//  Copyright © 2020 Jake Ridgway (student LM). All rights reserved.
+//
+
+import Foundation
---
+++ Quleo/GameViewController1.swift
@@ -0,0 +1,51 @@
+//
+//  GameViewController.swift
+// Quleo
+//
+//  Created by Asher Dayanim (student LM) on 1/15/20.
+//  Copyright © 2020 Asher Dayanim (student LM). All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene1") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .resizeFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            //view.showsFPS = true
+          //  view.showsNodeCount = true
+           // view.showsPhysics = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
