a583d49e6dae778f916cf985d37cea6b3c7471ee
Jake Ridgway <jakeridgway@gmail.com>
2020-02-20 08:46:16 -0500

    Initial Commit

---
+++ Quleo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quleo
+//
+//  Created by Jake Ridgway (student LM) on 2/20/20.
+//  Copyright © 2020 Jake Ridgway (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
