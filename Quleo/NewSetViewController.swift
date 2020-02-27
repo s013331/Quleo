0c839bf6b6ab6ef67299bebd171921a4816d4224
Fiona <cecerae@comcast.net>
2020-02-27 09:59:07 -0500

    Basic storyboard done

---
+++ Quleo/NewSetViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewSetViewController.swift
+//  Quleo
+//
+//  Created by Fiona Rae (student LM) on 2/27/20.
+//  Copyright © 2020 Jake Ridgway (student LM). All rights reserved.
+//
+
+import UIKit
+
+class NewSetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
